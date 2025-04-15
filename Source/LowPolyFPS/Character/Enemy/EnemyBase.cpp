4a626058e076c275b03865120c87c83ca00c893c
zakumann <samuel742able@gmail.com>
2025-04-15 23:47:56 +0900

    Add Enemy Character Code and Nav Mesh volume

--- Source/LowPolyFPS/Character/Enemy/EnemyBase.cpp
+++ Source/LowPolyFPS/Character/Enemy/EnemyBase.cpp
@@ -6,8 +6,6 @@
 // Sets default values
 AEnemyBase::AEnemyBase()
 {
- 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
 
 }
 
@@ -17,18 +15,3 @@ void AEnemyBase::BeginPlay()
 	Super::BeginPlay();
 	
 }
-
-// Called every frame
-void AEnemyBase::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
-
-}
-
-// Called to bind functionality to input
-void AEnemyBase::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
-{
-	Super::SetupPlayerInputComponent(PlayerInputComponent);
-
-}
-
